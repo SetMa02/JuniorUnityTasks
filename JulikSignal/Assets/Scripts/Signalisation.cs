7a53952fddcd26b51fb998afddf79adb0d66a239
Максим Майнагашев <maxuser99@mail.ru>
2022-05-07 22:04:39 +0700

    Fixed

--- JulikSignal/Assets/Scripts/Signalisation.cs
+++ JulikSignal/Assets/Scripts/Signalisation.cs
@@ -8,7 +8,9 @@ public class Signalisation : MonoBehaviour
 {
     [SerializeField] private float _peroidiciy = 3;
     [SerializeField] private Door _door;
-    
+
+    private IEnumerator _startSignalisationManager;
+    private IEnumerator _stopSignalisationManager;
     private AudioSource _audioSource;
     private float _minVolume = 0;
     private float _maxVolume = 1;
@@ -16,31 +18,45 @@ public class Signalisation : MonoBehaviour
     private void Start()
     {
         _audioSource = GetComponent<AudioSource>();
+        _startSignalisationManager = ManageSignalisation(_maxVolume);
+        _stopSignalisationManager = ManageSignalisation(_minVolume);
     }
 
     private void OnEnable()
     {
-        _door.ThiefEntered += ThiefDetected;
-        _door.ThiefExit += ThiefLeave;
+        _door.ThiefEntered += PlaySignalisation;
+        _door.ThiefExit += StopSignalisation;
     }
 
     private void OnDisable()
     {
-        _door.ThiefEntered -= ThiefDetected;
-        _door.ThiefExit -= ThiefLeave;
+        _door.ThiefEntered -= PlaySignalisation;
+        _door.ThiefExit -= StopSignalisation;
     }
 
-    private void ThiefDetected()
+    private void PlaySignalisation()
     {
-        StartCoroutine(ManageSignalisation(_maxVolume));
         _audioSource.Play();
+        if (_stopSignalisationManager != null)
+        {
+            StopCoroutine(_stopSignalisationManager);
+        }
+
+        StartCoroutine(_startSignalisationManager);
+        
     }
 
-    private void ThiefLeave()
+    private void StopSignalisation()
     {
-        StartCoroutine(ManageSignalisation(_minVolume));
+        if (_startSignalisationManager != null)
+        {
+            StopCoroutine(_startSignalisationManager);
+        }
+
+        StartCoroutine(_stopSignalisationManager);
+
     }
-    
+
     private IEnumerator ManageSignalisation(float targetValue)
     {
         while (_audioSource.volume != targetValue) 
