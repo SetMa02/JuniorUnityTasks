879fcfa9a5719e05349ed462cff292dae205f1a4
Максим Майнагашев <maxuser99@mail.ru>
2022-06-20 13:34:00 +0700

    Fixed

---
+++ Spawner/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class EnemySpawner : MonoBehaviour
+    {
+        [SerializeField] private Enemy _enemy;
+        [SerializeField] private EnemySpawner _enemySpawnerWest;
+        [SerializeField] private EnemySpawner _enemySpawnerEast;
+        [SerializeField] private float _delay;
+
+        private void Start()
+        {
+            StartCoroutine(Spawn());
+        }
+
+        public void SpawnEnemy()
+        {
+            GameObject.Instantiate(_enemy, gameObject.transform, false);
+        }
+        
+        private IEnumerator Spawn()
+        {
+            WaitForSeconds delay = new WaitForSeconds(_delay);
+            while (true)
+            {
+                _enemySpawnerWest.SpawnEnemy();
+                yield return delay;
+                _enemySpawnerEast.SpawnEnemy();
+                yield return delay;
+            }
+        }
+    }
+}
